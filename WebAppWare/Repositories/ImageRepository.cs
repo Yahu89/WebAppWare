8e1ede5cd3542210ca1e76bd48981741b096e3e4
Yahu89 <krzysztof.wohlert@gmail.com>
2024-02-29 17:54:31 +0100

    update

--- WebAppWare/Repositories/ImageRepository.cs
+++ WebAppWare/Repositories/ImageRepository.cs
@@ -26,33 +26,67 @@ namespace WebAppWare.Repositories
 
         public async Task Create(ProductModel product)
 		{
-			var absolutePath = CreateImageAbsolutePath(product);
+			int? id = null;
 
-			using var stream = new FileStream(absolutePath, FileMode.Create);
-
-			// zapisyjemy do folderu wwwroot
-			await product.ImageFile.CopyToAsync(stream);
-
-			// zapisujem w bazie dane zapisanego pliku
-			var image = new Image()
+			if (product.ImageFile != null)
 			{
-				Name = $"{product.ImageFile.Name}",
-				AbsolutePath = absolutePath,
-				Path = $"images/{product.ImageFile.Name}"
-			};
+				var localPath = CreateLocalPath(product);
 
-			await _dbContext.Images.AddAsync(image);
-			await _dbContext.SaveChangesAsync();
+				var image = new Image()
+				{
+					Name = $"{product.ImageFile.Name}",
+					Path = localPath,
+					AbsolutePath = Path.Combine(_webHostEnvironment.WebRootPath, localPath)
+				};
 
-			await _productRepo.Add(product, image.Id);
+				//var absolutePath = Path.Combine(_webHostEnvironment.WebRootPath, localPath);
+
+				using var stream = new FileStream(image.AbsolutePath, FileMode.Create);
+				await product.ImageFile.CopyToAsync(stream);
+
+				//var absolutePath = CreateImageAbsolutePath(product);
+
+				// zapisyjemy do folderu wwwroot
+				
+
+				// zapisujem w bazie dane zapisanego pliku
+				//var image = new Image()
+				//{
+				//	Name = $"{product.ImageFile.Name}",
+				//	AbsolutePath = absolutePath,
+				//	Path = $"images/{product.ImageFile.Name}"
+				//};
+
+				//image.AbsolutePath = absolutePath;
+
+				
+
+				await _dbContext.Images.AddAsync(image);
+				await _dbContext.SaveChangesAsync();
+
+				id = image.Id;
+			}
+			
+			await _productRepo.Add(product, id);
 		}
 
-		private string CreateImageAbsolutePath(BaseImageModel model)
+		//private string CreateImageAbsolutePath(BaseImageModel? model)
+		//{
+		//	//string? fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
+		//	//string? extension = Path.GetExtension(model.ImageFile.FileName);
+		//	//fileName = fileName + DateTime.Now.ToString("yymmssffff") + extension;
+		//	string fileName = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+
+		//	return fileName;
+		//}
+
+		private string CreateLocalPath(BaseImageModel? model)
 		{
-			string fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
-			string extension = Path.GetExtension(model.ImageFile.FileName);
+			string? fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
+			string? extension = Path.GetExtension(model.ImageFile.FileName);
 			fileName = fileName + DateTime.Now.ToString("yymmssffff") + extension;
-			fileName = Path.Combine(_webHostEnvironment.ContentRootPath, "images", fileName);
+			//fileName = Path.Combine("images", fileName);
+			fileName = $"images/{fileName}";
 
 			return fileName;
 		}
